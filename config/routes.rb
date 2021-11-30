d9c1a4610ff4472b60f9e03582dd50930ecfe26f
Agnieszka Dutkiewicz <a.dutkiewicz27@gmail.com>
2021-11-30 10:46:43 +0100

    Add links in items show

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   get 'item_versions/index'
   get 'item_versions/show'
-  get 'iten_versions/index'
-  get 'iten_versions/show'
   get 'orders/index'
   get 'orders/show'
-  get 'order/index'
-  get 'order/show'
+  get 'items/index'
+  get 'items/show'
   resources :clients
   resources :items
   resources :itemversions
