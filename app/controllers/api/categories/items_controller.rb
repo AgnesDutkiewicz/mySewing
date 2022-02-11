735ac3892f625cfedb2cdfbb2911d7059b0b37e2
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-02-11 12:51:05 +0100

    Updated filtered Items endpoint

--- app/controllers/api/categories/items_controller.rb
+++ app/controllers/api/categories/items_controller.rb
@@ -2,7 +2,8 @@ module Api
   module Categories
     class ItemsController < ApiController
       def index
-        chosen_item = Item.where(category: params[:category_id])
+        # chosen_item = Item.where(category: params[:category_id]) też działa
+        chosen_item = Item.all.select { |i| i.category.downcase == params[:category_id] }
 
         render json: chosen_item
       end
