bbb410bb1a40c031fa9e3e954b6e705e2af76970
Emilia Gwizdała <59012115+EGwizdala@users.noreply.github.com>
2022-02-15 11:42:31 +0100

    Add Access-Control-Allow-Origin '*'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # No idea what Action Cable is, but there's a slight chance this may add Access-Control-Allow-Origin: '*' to the response
+  config.action_cable.allowed_request_origins = ['*']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
