113c4c206c8f3185cd6f8c61f267b9f99904c24c
AgnesDutkiewicz <a.dutkiewicz27@gmail.com>
2022-02-22 10:20:10 +0100

    Revert "Fix CORS Attempt 6"
    This reverts commit 2383adc77916e2c0cf789e130f6e1787b3ea87bb.

--- config/application.rb
+++ config/application.rb
@@ -35,4 +35,11 @@ module MySewing
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
+  
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', :headers => :any, :methods => [:get, :post, :options]
+    end
+  end
 end
